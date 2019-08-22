c0047edd48c9b7a4d488b8fc7106d310e6ec502e
Brian Eckenrode <be3612@gmail.com>
2019-08-22 15:58:12 +0100

    minor mobile fixes

--- javascript/shiny.js
+++ javascript/shiny.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
 	/* TODO
-	 * Add support for pre-Gen6 games (ie. odds over 8192, any older methods)
-	 * Implement Radar and Dexnav
+	 * Implement Radar
 	 */
 
 	// For math
-	var pulls;
+	var pulls  = 0;
 	var trials = 0;
-	var denom = 4096;
+	var denom  = 4096;
 	var cumulative = 0.0;
+
 	var totalPulls = document.getElementById('total-pulls');
 	var denomElement = document.getElementById('denominator');
 	var totalPullsEstimate = document.getElementById('total-pulls-estimate');
@@ -34,55 +34,135 @@ $(document).ready(function() {
 
 		charm = $('.shiny-charm').is(':checked');
 		masuda = $('#masuda-method').is(':checked');
+		trials = parseInt($('.chain').val());
 
 		switch (tab) {
-			case (0) : // Safari
+			/*  Friend Safari odds
+			 *  Simple, static odds based on shiny charm
+			 */
+			case (0) :
 				pulls = (charm ? 7 : 5);
-				trials = parseInt($('#safari-length').val());
 				break;
+			/*  Breeding odds
+			 *  Odds differ based on shiny charm and masuda method
+			 *  4 different possible states
+			 */
 			case (1) : // Breeding
 				pulls = 1;
 				if (charm) pulls += 2;
 				if (masuda) {
 					pulls += (gen == 4 ? 4 : 5);
 				}
-				trials = parseInt($('#breeding-length').val());
 				break;
-			case (2) : // Fishing
-				var chain = $('#fishing-length').val();
+			/*  Chain Fishing odds
+			 *	Each encounter gets 1 + (2 * <chain length>) pulls
+			 *  Gets 2 additional pulls for the shiny charm
+			 */
+			case (2) :
+				var chain = trials;
 				chain = (chain > 20 ? 20 : chain);
 				pulls = 1 + (chain * 2) + (charm ? 2 : 0);
-				trials = parseInt($('#fishing-length').val());
 				break;
-			case (3) : // Hordes
+			/* 	Horde odds
+			 * 	Simple, static odds based on shiny charm
+			 */
+			case (3) :
 				pulls = (charm ? 15 : 5);
-				trials = parseInt($('#horde-length').val());
 				break;
-				break;
-			case (4) : // SOS
-				var chain = $('#sos-length').val();
+			/*	SOS Battle odds
+			 *	After a chain of 70 encounters, encounters get 3 additional pulls
+			 *  In SM, chain counter rolls over at 255 (8 bit counter)
+			 *  Gets 2 additional pulls for the shiny charm
+			 */
+			case (4) :
+				var chain = trials;
 				if($('#game-selector option[value=SM]').prop('selected')) chain %= 255;
-				// if($('input:radio[name=game]:checked').val() == 0) chain %= 255;
 				pulls = (chain >= 70 ? 4 : 1);
 				pulls += (charm ? 2 : 0);
-				trials = parseInt($('#sos-length').val());
 				break;
-			case (5) : // TODO Radar
+			/* 	Radar odds
+			 *	Odds of a particular patch of grass having a shiny follow a set formula
+			 *  This formula runs up to 4 times based on how many patches shake
+			 *  TODO Find some way to average odds based on 1 - 4 shaking patches
+			 */
+			case (5) :
+				denom = 65536;
+				chain = (trials > 40 ? 40 : trials);
+				pulls = Math.ceil(65535 / (8200 - (chain * 200)));
+
+				var singleGrassProb = binomial(1, 1, (pulls / denom));
+				
+				var totalProb = (0.25 * singleGrassProb) +
+					(0.25 * (singleGrassProb * 2)) + 
+					(0.25 * (singleGrassProb * 3)) +
+					(0.25 * (singleGrassProb * 4));
+
+				totalPullsEstimate.innerHTML = (1 / totalProb).toFixed(0);
+
 				break;
-			case (6) : // TODO Dexnav
+			/*	Dexnav odds
+			 *	Attempts to generate a shiny PID based on a complex formula
+			 *  Formula a function of the search level and the chain length
+			 *  If this formula fails, attempts a shiny PID based on standard odds
+			 */
+			case (6) :
+				var searchLevel = parseInt($('.search-level').val());
+				if (!searchLevel) searchLevel = 0;
+				denom = 10000;
+
+				if(searchLevel != 0) {
+					if(searchLevel > 100) {
+						if(searchLevel > 200) {
+							pulls = 800 + (searchLevel - 200);
+						}
+						else {
+							pulls = 600 + ((searchLevel - 100) * 2);
+						}
+					}
+					else {
+						pulls = searchLevel * 6;
+					}
+					pulls *= .01;
+				}
+				else {
+					pulls = 0;
+				}
+
+				pulls = Math.ceil(pulls);
+
+				var chainBonus = (trials == 100 ? 10 : (trials == 50 ? 5 : 0));
+				var charmBonus = (charm ? 2 : 0);
+
+				var n = 1 + chainBonus + charmBonus;
+				
+				var dexnavProbBonus   = binomial(1, n + 4, (pulls / denom));
+				var dexnavProbNoBonus = binomial(1, n, (pulls / denom));
+				var normalProb        = binomial(1, 1, (1 + charmBonus) / 4096);
+
+				var totalProbBonus    = ((1 - dexnavProbBonus) * normalProb) + dexnavProbBonus;
+				var totalProbNoBonus  = ((1 - dexnavProbNoBonus) * normalProb) + dexnavProbNoBonus;
+
+				var totalProb         = (0.04 * totalProbBonus) + (0.96 * totalProbNoBonus);
+
+				totalPullsEstimate.innerHTML = (1 / totalProb).toFixed(0);
+
 				break;
-			case (7) : // Full Odds
+			/* 	Full Odds
+			 *	Simple, static odds based on shiny charm
+			 */
+			case (7) :
 				pulls = (charm ? 3 : 1);
-				trials = parseInt($('#full-odds-length').val());
 				break;
 			default :
 				break;
 		}
 
 		// Display
+		if(tab != 5 && tab != 6) {
+			totalPullsEstimate.innerHTML = Math.round(denom / pulls);
+		}
 		totalPulls.innerHTML = pulls;
 		denomElement.innerHTML = denom;
-		totalPullsEstimate.innerHTML = Math.round(denom / pulls);
 		cumulative = (binomial(1, trials, (pulls / denom)).toFixed(12));
 		totalCumulative.innerHTML = cumulative;
 		totalPercent.innerHTML = (cumulative * 100).toFixed(2);
@@ -141,13 +221,16 @@ $(document).ready(function() {
 	})
 
 	$('#game-selector').change(function() {
+
 		$('.shiny-charm').prop('checked', false);
 		$('#masuda-method').prop('checked', false);
 		$('.counter').prop('value', 0);
+		
 		Array.from(document.getElementsByClassName('tab')).forEach((tab) => {
 			$(tab).hide();
 		});
 		var gameId = $(this).find("option:selected").attr("value");
+		
 		switch(gameId) {
 			case "RSE"  :
 				$('#full-odds-tab').show().children().click();
@@ -203,18 +286,22 @@ $(document).ready(function() {
 
 	// Increment the encounters
 	function increment() {
-		console.log('Counter increment');
-		var counter = $(this).parent().siblings('.counter');
-		counter.val(parseInt(counter.val()) + 1);
+		document.getElementsByClassName('counter')[0].stepUp();
 		updateValue();
 	}
 
 	// Reset the encounters
 	function reset() {
-		console.log('Counter reset');
-		$(this).parent().siblings('.counter').prop('value', 0);
+		document.getElementsByClassName('counter')[0].value = 0;
 		updateValue();
 	}
+
+	$(document).on('click', '.btn-increment', increment);
+	$(document).on('click', '.btn-reset', reset);
+	$(document).on('change', '.shiny-charm', updateValue);
+	$(document).on('change', '.counter', updateValue);
+	$(document).on('change', '#masuda-method', updateValue);
+	$(document).on('change', '#game-select', updateValue);
 
 	// Format Pokemon selector
 	function formatOption(state) {
@@ -226,12 +313,6 @@ $(document).ready(function() {
 		);
 		return $state;
 	};
-
-	$(document).on('click', '.btn-increment', increment);
-	$(document).on('click', '.btn-reset', reset);
-	$(document).on('change', '.shiny-charm', updateValue);
-	$(document).on('change', '#masuda-method', updateValue);
-	$(document).on('change', '#game-select', updateValue);
 
 	$('#pokemon-selector').select2({
 		templateResult: formatOption
